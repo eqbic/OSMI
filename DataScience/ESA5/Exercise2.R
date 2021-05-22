44ac286f21babdced4012050c413ef4a1609bb56
eqbic <erichquerner@posteo.org>
2021-05-22 14:36:26 +0200

    finish exercise 2

--- DataScience/ESA5/Exercise2.R
+++ DataScience/ESA5/Exercise2.R
@@ -1,1 +1,12 @@
+data()
 esoph
+summary(esoph)
+
+# show the number of cases of alcohol consumption by age
+table(esoph$agegp, esoph$alcgp)
+
+# show the number of cases of tobacco consumption by age
+table(esoph$agegp, esoph$tobgp)
+
+# average number of cases per age group
+tapply(esoph$ncases, esoph$agegp, mean)
