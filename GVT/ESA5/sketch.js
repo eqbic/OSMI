0a2b4427e8418057ac1b973605de1f25eaac7815
eqbic <erichquerner@posteo.org>
2021-11-21 20:54:58 +0100

    finished esa5

--- GVT/ESA5/sketch.js
+++ GVT/ESA5/sketch.js
@@ -11,37 +11,23 @@ function getShaderSource(url) {
     return (req.status == 200) ? req.responseText : null;
 };
 
-class Vertex {
-    #index
-    #position;
-    constructor(index, position) {
-        this.#index = index;
-        this.#position = position;
-    }
-
-    get position() {
-        return this.#position;
-    }
-
-    get index() {
-        return this.#index;
-    }
-}
 
 class Mesh {
     constructor() {
         this._vertices = [];
         this._indices = [];
         this._lineIndices = [];
+        this._faces = [];
         this._positionBuffer = gl.createBuffer();
         this._indexBuffer = gl.createBuffer();
         this._lineBuffer = gl.createBuffer();
+        this._normalBuffer = gl.createBuffer();
     }
-
+    
     draw(shaderProgram, solidColor, lineColor) {
-
+        
     }
-
+    
     setup() { }
 }
 
@@ -53,7 +39,7 @@ class Torus extends Mesh {
         this._innerRadius = innerRadius;
         this.setup();
     }
-
+    
     setup() {
         console.log("TORUS SETUP");
         const step = 10.0 / this._resolution;
@@ -78,18 +64,18 @@ class Torus extends Mesh {
             positions.push(pos_y);
             positions.push(pos_z);
         }
-
+        
         for (var i = 0; i < 2 * (this._resolution - 1) * (this._resolution - 1); i++) {
             if ((i + 1) % this._resolution === 0) continue;
             this._indices.push(i);
             this._indices.push(i + this._resolution);
             this._indices.push(i + this._resolution + 1);
-
+            
             this._indices.push(i);
             this._indices.push(i + 1);
             this._indices.push(i + 1 + this._resolution);
         }
-
+        
         for (var i = 0; i < 2 * (this._resolution - 1) * (this._resolution - 1); i++) {
             if ((i + 1) % this._resolution == 0) {
                 this._lineIndices.push(i);
@@ -101,31 +87,31 @@ class Torus extends Mesh {
             this._lineIndices.push(i);
             this._lineIndices.push(i + this._resolution);
         }
-
+        
         gl.bindBuffer(gl.ARRAY_BUFFER, this._positionBuffer);
         gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
-
+        
         gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this._indexBuffer);
         gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(this._indices), gl.STATIC_DRAW);
-
+        
         gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this._lineBuffer);
         gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(this._lineIndices), gl.STATIC_DRAW);
     }
-
+    
     draw(shaderProgram, solidColor, lineColor) {
         gl.bindBuffer(gl.ARRAY_BUFFER, this._positionBuffer);
         const posAttrib = gl.getAttribLocation(shaderProgram, "aVertexPosition");
         gl.vertexAttribPointer(posAttrib, 3, gl.FLOAT, false, 0, 0);
         gl.enableVertexAttribArray(posAttrib);
-
+        
         const vertexCount = 3 * 2 * (this._resolution - 1) * (this._resolution - 1);
         const type = gl.UNSIGNED_SHORT;
         const uColor = gl.getUniformLocation(shaderProgram, "color");
-
+        
         gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this._indexBuffer);
         gl.uniform4fv(uColor, solidColor);
         gl.drawElements(gl.TRIANGLES, vertexCount, type, 0);
-
+        
         gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this._lineBuffer);
         gl.uniform4fv(uColor, lineColor);
         gl.drawElements(gl.LINES, (4 * (this._resolution - 1) * (this._resolution - 1) + 2 * this._resolution), type, 0);
@@ -140,7 +126,7 @@ class Grid extends Mesh {
         this._height = height;
         this.setup();
     }
-
+    
     setup() {
         console.log("GRID SETUP");
         const step = 2.0 / this._resolution;
@@ -152,109 +138,188 @@ class Grid extends Mesh {
                 x = 0;
                 z += 1;
             }
-
+            
             pos_x = this._width * ((x + 0.5) * step - 1.0);
             pos_y = 0.0;
             pos_z = this._height * ((z + 0.5) * step - 1.0);
-
+            
             vec3.set(position, pos_x, pos_y, pos_z);
             this._vertices.push(new Vertex(i, position));
             positions.push(pos_x);
             positions.push(pos_y);
             positions.push(pos_z);
         }
-
+        
         for (var i = 0; i < 2 * (this._resolution - 1) * (this._resolution - 1); i++) {
             if ((i + 1) % this._resolution === 0) {
                 this._lineIndices.push(i);
                 this._lineIndices.push(i + this._resolution);
                 continue;
             }
-
+            
             this._indices.push(i);
             this._indices.push(i + this._resolution);
             this._indices.push(i + this._resolution + 1);
-
+            
             this._indices.push(i);
             this._indices.push(i + 1);
             this._indices.push(i + 1 + this._resolution);
-
+            
             this._lineIndices.push(i);
             this._lineIndices.push(i + 1);
             this._lineIndices.push(i);
             this._lineIndices.push(i + this._resolution);
         }
-
+        
         gl.bindBuffer(gl.ARRAY_BUFFER, this._positionBuffer);
         gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
-
+        
         gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this._indexBuffer);
         gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(this._indices), gl.STATIC_DRAW);
-
+        
         gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this._lineBuffer);
         gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(this._lineIndices), gl.STATIC_DRAW);
     }
-
+    
     draw(shaderProgram, solidColor, lineColor) {
         gl.bindBuffer(gl.ARRAY_BUFFER, this._positionBuffer);
         const posAttrib = gl.getAttribLocation(shaderProgram, "aVertexPosition");
         gl.vertexAttribPointer(posAttrib, 3, gl.FLOAT, false, 0, 0);
         gl.enableVertexAttribArray(posAttrib);
-
+        
         const vertexCount = 3 * 2 * (this._resolution - 1) * (this._resolution - 1);
         const type = gl.UNSIGNED_SHORT;
         const uColor = gl.getUniformLocation(shaderProgram, "color");
-
+        
         gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this._indexBuffer);
         gl.uniform4fv(uColor, solidColor);
         gl.drawElements(gl.TRIANGLES, vertexCount, type, 0);
-
+        
         gl.uniform4fv(uColor, lineColor);
         gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this._lineBuffer);
         gl.drawElements(gl.LINES, (4 * (this._resolution - 1) * (this._resolution - 1) + 2 * this._resolution), type, 0);
     }
 }
+class Vertex {
+    constructor(index, position) {
+        this._index = index;
+        this._position = position;
+        this._normal = vec3.create();
+    }
 
+    get position() {
+        return this._position;
+    }
+
+    get index() {
+        return this._index;
+    }
+
+    set normal(normal){
+        this._normal = normal;
+    }
+    
+    get normal(){
+        return this._normal;
+    }
+}
+
+class Face {
+    constructor(index, v0,v1,v2){
+        this._index = index;
+        this._v0 = v0;
+        this._v1 = v1;
+        this._v2 = v2;
+        this._normal = this.calculateNormal();
+    }
+
+    calculateNormal(){
+        let normal = vec3.create();
+        let edge1 = vec3.create();
+        vec3.subtract(edge1, this._v1.position, this._v0.position);
+        let edge2 = vec3.create();
+        vec3.subtract(edge2, this._v2.position, this._v0.position);
+        vec3.cross(normal, edge1, edge2);
+        vec3.normalize(normal, normal);
+        return normal;
+    }
+
+    
+}
 class Cube extends Mesh {
     constructor(size) {
         super();
         this._size = size;
         this.setup();
     }
-
+    
     setup() {
         console.log("CUBE SETUP");
-
+        
 
         let positions = [
+            // Front face
+            -1.0, -1.0,  1.0,
+             1.0, -1.0,  1.0,
+             1.0,  1.0,  1.0,
+            -1.0,  1.0,  1.0,
+          
+            // Back face
             -1.0, -1.0, -1.0,
-            -1.0, 1.0, -1.0,
-            1.0, 1.0, -1.0,
-            1.0, -1.0, -1.0,
-            -1.0, -1.0, 1.0,
-            -1.0, 1.0, 1.0,
-            1.0, 1.0, 1.0,
-            1.0, -1.0, 1.0
-        ];
+            -1.0,  1.0, -1.0,
+             1.0,  1.0, -1.0,
+             1.0, -1.0, -1.0,
+          
+            // Top face
+            -1.0,  1.0, -1.0,
+            -1.0,  1.0,  1.0,
+             1.0,  1.0,  1.0,
+             1.0,  1.0, -1.0,
+          
+            // Bottom face
+            -1.0, -1.0, -1.0,
+             1.0, -1.0, -1.0,
+             1.0, -1.0,  1.0,
+            -1.0, -1.0,  1.0,
+          
+            // Right face
+             1.0, -1.0, -1.0,
+             1.0,  1.0, -1.0,
+             1.0,  1.0,  1.0,
+             1.0, -1.0,  1.0,
+          
+            // Left face
+            -1.0, -1.0, -1.0,
+            -1.0, -1.0,  1.0,
+            -1.0,  1.0,  1.0,
+            -1.0,  1.0, -1.0,
+          ];
+          
 
         for (var i = 0; i < positions.length; i++) {
             positions[i] *= this._size;
+
+            if(i % 3 === 1) positions[i] += 1;
+            if(i %3 === 0) positions[i] += 1;
         }
 
         this._indices = [
-            0, 1, 2,
-            0, 2, 3,
-            2, 3, 6,
-            3, 6, 7,
-            4, 5, 6,
-            4, 6, 7,
-            0, 1, 5,
-            0, 5, 4,
-            5, 1, 2,
-            5, 2, 6,
-            4, 0, 3,
-            4, 3, 7
-        ];
+            0,  1,  2,      0,  2,  3,    // front
+            4,  5,  6,      4,  6,  7,    // back
+            8,  9,  10,     8,  10, 11,   // top
+            12, 13, 14,     12, 14, 15,   // bottom
+            16, 17, 18,     16, 18, 19,   // right
+            20, 21, 22,     20, 22, 23,   // left
+          ];
+
+        for(var i = 0; i < this._indices.length; i+=3){
+            let p0 = new Vertex(i, [positions[this._indices[i]], positions[this._indices[i] + 1], positions[this._indices[i] + 2]]);
+            let p1 = new Vertex(i + 1, [positions[this._indices[i + 1]], positions[this._indices[i + 1] + 1], positions[this._indices[i + 1] + 2]]);
+            let p2 = new Vertex(i + 2, [positions[this._indices[i + 2]], positions[this._indices[i + 2] + 1], positions[this._indices[i + 2] + 2]]);
+            let index = i / 3;
+            let face = new Face(index, p0, p1, p2);
+            this._faces.push(face);
+        }
 
         this._lineIndices = [
             0, 1,
@@ -266,9 +331,9 @@ class Cube extends Mesh {
             6, 7,
             7, 4,
             0, 4,
-            1, 5,
+            1, 7,
             2, 6,
-            3, 7
+            3, 5
         ];
 
         gl.bindBuffer(gl.ARRAY_BUFFER, this._positionBuffer);
@@ -351,6 +416,14 @@ class Camera {
     move(direction) {
         mat4.translate(this._modelViewMatrix, this._modelViewMatrix, direction);
         mat4.getTranslation(this._position, this._modelViewMatrix);
+        mat4.invert(this._inverseModelViewMatrix, this._modelViewMatrix);
+    }
+
+    moveWorld(direction){
+        this._modelViewMatrix[12] += direction[0];
+        this._modelViewMatrix[14] += direction[2];
+        mat4.getTranslation(this._position, this._modelViewMatrix);
+        this.lookAtPosition([0,0,0]);
         mat4.invert(this._inverseModelViewMatrix, this._modelViewMatrix);
     }
     
@@ -446,7 +519,7 @@ function main() {
 
     scene.addMesh(new Grid(10, 4, 4));
     scene.addMesh(new Torus(150, 0.5, 0.25));
-    scene.addMesh(new Cube(0.2));
+    scene.addMesh(new Cube(0.5));
     scene.draw(shaderProgram, solidColor, lineColor);
 
     console.log(scene.camera.forward);
@@ -476,6 +549,21 @@ function main() {
             scene.camera.changeHeight(-0.1);
         }
 
+        if(e.key === 'w'){
+            scene.camera.moveWorld([0,0,-0.1])
+        }
+
+        if(e.key === 's'){
+            scene.camera.moveWorld([0,0,0.1]);
+        }
+
+        if(e.key === 'a'){
+            scene.camera.moveWorld([-0.1, 0,0]);
+        }
+
+        if(e.key === 'd'){
+            scene.camera.moveWorld([0.1,0,0]);
+        }
         gl.uniformMatrix4fv(projMat, false, scene.camera.projection);
         
         gl.uniformMatrix4fv(viewMat, false, scene.camera.inverseView);
