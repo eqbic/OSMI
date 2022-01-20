46bcab94353a42cb1be0296cf015b3cc580fcc3a
eqbic <erichquerner@posteo.org>
2022-01-20 09:58:55 +0100

    general changes

--- GVT/ESA9/main.js
+++ GVT/ESA9/main.js
@@ -2,12 +2,19 @@ import { Canvas } from "./Core/Canvas.js";
 import { Shader } from "./Core/Shader.js";
 import { Scene } from "./Core/Scene.js";
 import { Animator } from "./Animation/Animator.js";
-import { Grid } from "./Graphics/Meshes/Grid.js";
-import { MeshLoader } from "./Graphics/Meshes/MeshLoader.js";
+import { GridMesh } from "./Graphics/Meshes/GridMesh.js";
+import { ObjMesh } from "./Graphics/Meshes/ObjMesh.js";
 import { PointLight } from "./Graphics/Lights/PointLight.js";
-import { Sphere } from "./Graphics/Meshes/Sphere.js";
-import { Torus } from "./Graphics/Meshes/Torus.js";
+import { SphereMesh } from "./Graphics/Meshes/SphereMesh.js";
+import { TorusMesh } from "./Graphics/Meshes/TorusMesh.js";
 import {Colors} from "./Utils/Colors.js";
+import {ModelBase} from "./Graphics/Models/ModelBase.js";
+import {Material} from "./Graphics/Materials/Material.js";
+import {Cube} from "./Graphics/Models/Cube.js";
+import {FileModel} from "./Graphics/Models/FileModel.js";
+import {Grid} from "./Graphics/Models/Grid.js";
+import {Torus} from "./Graphics/Models/Torus.js";
+import {Sphere} from "./Graphics/Models/Sphere.js";
 
 const container = document.getElementById('renderer');
 let canvas = new Canvas("glCanvas", container);
@@ -18,38 +25,27 @@ const shaderPath = "Graphics/Shaders/";
 const modelPath = "Resources/Models/";
 
 const toonShader = new Shader(gl, shaderPath + "toon.vs", shaderPath + "toon.fs");
-const phongShader = new Shader(gl, shaderPath + "PhongPoint.vs", shaderPath + "PhongPoint.fs");
+const phongPoint = new Shader(gl, shaderPath + "Phong.vs", shaderPath + "PhongPoint.fs");
+const phongDirectional = new Shader(gl, shaderPath + "Phong.vs", shaderPath + "PhongDirectional.fs");
 
 const scene = new Scene(gl,[0.1, 0.2, 0.3]);
 const animator = new Animator();
 
-// const floor = new Grid(gl, 3, phongShader, Colors.White);
-// scene.addMesh(floor);
-// floor.uniformscale(10);
+const wood = new Material(gl, phongPoint, Colors.White, "Resources/Textures/wood.jpg");
+const uv_pattern = new Material(gl, phongPoint, Colors.White, "Resources/Textures/uv_grid.jpg");
 
-// const torus1 = new Torus(gl, 32, 32, phongShader, Colors.White);
-// scene.addMesh(torus1);
-// torus1.Position = [-1.5, 1, 0];
-//
-// const torus2 = new Torus(gl, 32, 32, phongShader, Colors.White);
-// scene.addMesh(torus2);
-// torus2.Position = [-0, 1, 0];
-//
-// const torus3 = new Torus(gl, 32, 32, phongShader, Colors.White);
-// scene.addMesh(torus3);
-// torus3.Position = [1.5, 1, 0];
+const monkey = new FileModel(gl, modelPath + 'monkey_smooth.obj', uv_pattern);
+scene.addModel(monkey);
+monkey.Position = [0, 1, 0];
+monkey.uniformscale(0.5);
 
-const cube = new MeshLoader(gl, modelPath + 'monkey_smooth.obj', phongShader, Colors.Red);
-scene.addMesh(cube);
-
-cube.Position = [0, 1, 0];
-cube.uniformscale(0.5);
-
+const floor = new Grid(gl, 16, wood);
+scene.addModel(floor);
+floor.uniformscale(5);
 
 const light = new PointLight(Colors.White, 0.5);
 scene.addLight(light);
 light.Position = [0, 2, 1];
-
 
 function processInput(e) {
     switch (e.code) {
@@ -91,7 +87,7 @@ function draw() {
     // torus2.rotate([0, 0.5, 0]);
     // torus3.rotate([0, 0.5, 0.5]);
 
-    cube.rotate([0,0,1]);
+    monkey.rotate([0,0,1]);
     scene.draw();
     window.requestAnimationFrame(draw);
 }
