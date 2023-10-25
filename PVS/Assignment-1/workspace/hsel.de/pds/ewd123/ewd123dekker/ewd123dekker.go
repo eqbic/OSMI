65a0111615371978766cbc03face743c972555e9
eqbic <erichquerner@posteo.org>
2023-10-25 12:48:46 +0200

    reset first try

--- PVS/Assignment-1/workspace/hsel.de/pds/ewd123/ewd123dekker/ewd123dekker.go
+++ PVS/Assignment-1/workspace/hsel.de/pds/ewd123/ewd123dekker/ewd123dekker.go
@@ -14,12 +14,6 @@
 
 package ewd123dekker
 
-import (
-	"log"
-
-	"hsel.de/pds/ewd123/controller"
-)
-
 // global synchronization variables
 var c1, c2 = 1, 1
 var turn = 1
@@ -32,68 +26,10 @@ func Start() {
 
 // process1 simulates the behaviour of the first process
 func process1() {
-A1:
-	c1 = 0
-L1:
-	if c2 == 0 {
-		if turn == 1 {
-			goto L1
-		}
-		c1 = 1
-	B1:
-		if turn == 2 {
-			goto B1
-		}
-		goto A1
-	}
-
-	controller.EnterCriticalSection(1)
-	controller.InsideCriticalSection(1, 50)
-	controller.LeaveCriticalSection(1)
-
-	turn = 2
-	c1 = 1
-
-	controller.OutsideCriticalSection(1, 100)
-
-	if controller.ProcessCrashed(0.1) {
-		log.Printf("Process 1 crashed\n")
-		return
-	}
-
-	goto A1
+	// TO DO
 }
 
 // process2 simulates the behaviour of the second process
 func process2() {
-A2:
-	c2 = 0
-L2:
-	if c1 == 0 {
-		if turn == 2 {
-			goto L2
-		}
-		c2 = 1
-	B2:
-		if turn == 1 {
-			goto B2
-		}
-		goto A2
-	}
-
-	controller.EnterCriticalSection(2)
-	controller.InsideCriticalSection(2, 50)
-	controller.LeaveCriticalSection(2)
-
-	turn = 1
-	c2 = 1
-
-	controller.OutsideCriticalSection(2, 100)
-
-	if controller.ProcessCrashed(0.1) {
-		log.Printf("Process 2 crashed\n")
-		return
-	}
-
-	goto A2
+	// TO DO
 }
